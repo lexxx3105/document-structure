a58831038448263adcdc1205d45d4e30d7c5fc70
lexxx3105 <129284703+lexxx3105@users.noreply.github.com>
2023-10-18 21:36:39 +0300

    tooltip

---
+++ tooltip/task.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+    const tooltipElements = Array.from(document.getElementsByClassName('has-tooltip'));
+
+    let tooltip = document.createElement('div');
+    tooltip.classList.add('tooltip');
+    document.body.append(tooltip);
+
+    tooltipElements.forEach(element => {
+        element.addEventListener('click', event => {
+            event.preventDefault();
+
+            if (tooltip.classList.contains('tooltip_active') && tooltip.innerText === element.title) {
+                tooltip.classList.remove('tooltip_active');
+            } else {
+                tooltip.innerHTML = element.title;
+                let coords = element.getBoundingClientRect();
+
+                let left = coords.left + (element.offsetWidth - tooltip.offsetWidth) / 2;
+                if (left < 0) left = 0;
+
+                let top = coords.top - tooltip.offsetHeight - 5;
+                if (top < 0) {
+                    top = coords.top + element.offsetHeight + 5;
+                }
+
+                tooltip.style.left = `${left}px`;
+                tooltip.style.top = `${top}px`;
+                tooltip.classList.add('tooltip_active');
+            }
+        });
+    });
+};
